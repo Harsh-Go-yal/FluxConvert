e566ddc2563944e872ddb44977f23f83ccd8f6a7
Harsh-Go-yal <harshgoyal0807@gmail.com>
2025-11-29 04:54:45 +0530

    Add built WASM artifacts and Docker fixes

---
+++ packages/wasm/pkg/flux_wasm_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const merge_pdfs: (a: number, b: number) => [number, number, number, number];
+export const split_pdf: (a: number, b: number) => [number, number, number, number];
+export const compress_pdf: (a: number, b: number, c: number) => [number, number, number, number];
+export const resize_image: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const init_panic_hook: () => void;
+export const optimize_image: (a: number, b: number) => [number, number, number, number];
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_alloc: () => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_start: () => void;
